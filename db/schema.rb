cc7dc363a387a54290fc0fa4a21ced0da3044385
Phalla <phallaem@gmail.com>
2015-07-28 16:18:27 +0700

    Update Form and Navbar, rake DB

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20150728063408) do
 
   create_table "categories", force: :cascade do |t|
-    t.string   "Cate_name"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
