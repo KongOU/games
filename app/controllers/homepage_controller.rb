caf806fcadb1672b6ff2e984251f9459c5570725
Panh Phanith <panhphanith.kh@gmail.com>
2015-07-30 18:33:36 +0700

    fix bug in game controller and view

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,4 @@
 class HomepageController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     @games = Game.lastest_twently_game
     @weighted_average_game = Game.weighted_average_limit
